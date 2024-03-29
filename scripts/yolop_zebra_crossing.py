bbd9befac3eebe4e79c481a2a3145ae891110f9d
Łukasz Mędrek <lukasz@lukaszm.xyz>
2024-03-29 09:42:20 +0100

    feat: copy code from kaggle
    Co-authored-by: Igor Sosnowicz <igor.sosnowicz@gmail.com>

---
+++ scripts/yolop_zebra_crossing.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Model will be composed of pretrained weights except for the output layers
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', classes=5)
+#     1 osoby starsze 
+#     2 osoby z dziećmi
+#     3 osoby z wózkiem
+#     4 osoby z bagażem
+#     5 grupa uczniów
+
+# Lock weights of all layers except the last one
+for layer in model.parameters():
+    layer.auto_grad = False
+    
+# Unlock last layer's parameters
+
+# ...
