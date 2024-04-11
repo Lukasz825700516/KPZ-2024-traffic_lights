9f5bb2d07ce4f689aff7025cf6d6d9d887cc7a1f
dawid-sz22 <dawid.szelag22@gmail.com>
2024-04-11 22:33:54 +0200

    Counting average by frames added

--- scripts/live_demo_v2.py
+++ scripts/live_demo_v2.py
@@ -22,12 +22,50 @@ def main():
     
     prev_frame_time = 0
     new_frame_time = 0
+    count_frames = 0
+    
+    label_avg_dict = {
+        0 : 0.0,
+        1 : 0.0,
+        2 : 0.0,
+        3 : 0.0,
+        4 : 0.0,
+        5 : 0.0,
+        6 : 0.0
+    }
+    label_count_dict = {
+        0 : 0,
+        1 : 0,
+        2 : 0,
+        3 : 0,
+        4 : 0,
+        5 : 0,
+        6 : 0
+    }
   
     while cap.isOpened():
         res, frame = cap.read()
         results = model(frame)
         annotated_frame = results[0].plot()
         
+        # Count average to light green light
+        for result in results:
+            boxes = result.boxes.cpu().numpy()
+            for i in range (len(boxes.cls)):
+                label_avg_dict[int(boxes.cls[i])] += boxes.conf[i]
+                label_count_dict[int(boxes.cls[i])] += 1
+
+        if count_frames <= 10:
+            count_frames += 1
+        else:
+            for i in range (len(label_avg_dict)):
+                if label_count_dict[i]:
+                    label_avg_dict[i] /= label_count_dict[i]
+                if label_avg_dict[i] > 0.6:
+                    print("GREEN LIGHT FOR LABEL: " + str(i))
+                label_count_dict[i] = 0
+                label_avg_dict[i] = 0.0
+            
         font = cv2.FONT_HERSHEY_SIMPLEX 
         new_frame_time = time.time() 
     
@@ -43,7 +81,7 @@ def main():
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
         
-        # time.sleep(0.05)
+        # time.sleep(0.1)
         
 if __name__ == "__main__":
     main()
