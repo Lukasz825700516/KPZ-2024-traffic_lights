2016ab42b43784e5617172f13b182f41ab0be959
Iamhexi <igor.sosnowicz@gmail.com>
2024-04-11 10:21:46 +0200

    fix: copying dataset instead of moving

--- scripts/merge_datasets.py
+++ scripts/merge_datasets.py
@@ -1,87 +1,86 @@
-import os
 import sys
 import shutil
 from pathlib import Path
+import itertools
 
 class_for_wheelchair = 3
 class_for_blind = 4
 class_for_suitcase = 5
 
-dataset_dir = os.path.abspath(sys.argv[1])
-dataset_parent_directories = ['Blind', 'Suitcase', 'Wheelchair', 'Child_Elderly_Adult']
-dataset_directories = ['test', 'train', 'valid']
-subdirectories = ['images', 'labels']
+def validate_arguments():
+    program_usage = 'Usage: python create_dataset.py <dataset_directory> <copy_instead_move>=True'
+    available_options = [''.join(x) for x in itertools.product(['', 'copy_instead_move='], ['True', 'False', 'true', 'false', '1', '0']) ]
+    if len(sys.argv) not in (2, 3):
+        print(program_usage)
+        sys.exit(1)         
+    if len(sys.argv) == 3 and sys.argv[2] not in available_options:
+        print(itertools.product(['', 'copy_instead_move='], ['True', 'False', 'true', 'false', '1', '0']))
+        print(program_usage)
+        sys.exit(1)
 
-if len(sys.argv) != 2:
-    print('Usage: python create_dataset.py <dataset_directory>')
-    exit(1)
+def copy_or_move_files(src_dir, dest_dir, copy_instead_move):
+    if copy_instead_move:
+        shutil.copy(src_dir, dest_dir)
+    else:
+        shutil.move(src_dir, dest_dir)
 
+def modify_label_file(label_file, class_code):
+    with open(label_file, 'r') as f:
+        label_lines = f.readlines()
 
-# Consolidate path definitions
-train_image_dir = os.path.join(dataset_dir, 'train', 'images')
-test_image_dir = os.path.join(dataset_dir, 'test', 'images')
-valid_image_dir = os.path.join(dataset_dir, 'valid', 'images')
-train_label_dir = os.path.join(dataset_dir, 'train', 'labels')
-test_label_dir = os.path.join(dataset_dir, 'test', 'labels')
-valid_label_dir = os.path.join(dataset_dir, 'valid', 'labels')
+    modified_label_lines = []
+    for line in label_lines:
+        content = line.split()
+        content[0] = str(class_code)
+        modified_label_lines.append(' '.join(content) + '\n')
 
-# Create the directory structure
-for dataset_directory in dataset_directories:
-    for subdirectory in subdirectories:
-        path = Path(os.path.join(os.path.join(dataset_dir, dataset_directory), subdirectory))
-        path.mkdir(parents=True, exist_ok=True)
+    with open(label_file, 'w') as f:
+        f.writelines(modified_label_lines)
 
-for dataset_parent_directory in dataset_parent_directories:
-    path = os.path.join(dataset_dir, dataset_parent_directory)
+def main():
+    validate_arguments()
 
-    # enter subdirectory
-    subdir = os.listdir(path)
-    path = os.path.join(path, subdir[0]) 
+    dataset_dir = Path(sys.argv[1]).resolve()
+    copy_instead_move = len(sys.argv) == 3 and (sys.argv[2] in ('True', 'true', '1', 'copy_instead_move=True', 'copy_instead_move=true'))
 
+    dataset_parent_directories = ['Blind', 'Suitcase', 'Wheelchair', 'Child_Elderly_Adult']
+    dataset_directories = ['test', 'train', 'valid']
+    subdirectories = ['images', 'labels']
+
+    # Create directory structure
     for dataset_directory in dataset_directories:
-        new_path = os.path.join(path, dataset_directory)
+        for subdirectory in subdirectories:
+            (dataset_dir / dataset_directory / subdirectory).mkdir(parents=True, exist_ok=True)
 
-        image_path = os.path.join(new_path, 'images')
-        label_path = os.path.join(new_path, 'labels')        
+    for dataset_parent_directory in dataset_parent_directories:
+        parent_dir = dataset_dir / dataset_parent_directory
+        sub_dir = next(iter(parent_dir.iterdir()))
 
-        # move images to the corresponding directories
-        for image in os.listdir(image_path):
-            if dataset_directory == 'train':
-                os.rename(os.path.join(image_path, image), os.path.join(train_image_dir, image))
-            elif dataset_directory == 'test':
-                os.rename(os.path.join(image_path, image), os.path.join(test_image_dir, image))
-            elif dataset_directory == 'valid':
-                os.rename(os.path.join(image_path, image), os.path.join(valid_image_dir, image))
+        for dataset_directory in dataset_directories:
+            new_path = sub_dir / dataset_directory
+            image_dir = new_path / 'images'
+            label_dir = new_path / 'labels'
 
-        # reassign the classes and then move them to the corresponding directories
-        for label in os.listdir(label_path):
-            with open(os.path.join(label_path, label), 'r') as f:
-                label_lines = f.readlines()
+            for image_file in image_dir.iterdir():
+                dest_dir = dataset_dir / dataset_directory / 'images' / image_file.name
+                copy_or_move_files(image_file, dest_dir, copy_instead_move)
 
-            modified_label_lines = []
-            for line in label_lines:
-                content = line.split()
+            for label_file in label_dir.iterdir():
                 if dataset_parent_directory == 'Blind':
-                    content[0] = str(class_for_blind)
+                    modify_label_file(label_file, class_for_blind)
                 elif dataset_parent_directory == 'Suitcase':
-                    content[0] = str(class_for_suitcase)
+                    modify_label_file(label_file, class_for_suitcase)
                 elif dataset_parent_directory == 'Wheelchair':
-                    content[0] = str(class_for_wheelchair)
-                modified_label_lines.append(' '.join(content) + '\n')
+                    modify_label_file(label_file, class_for_wheelchair)
 
-            # reassign the classes
-            with open(os.path.join(label_path, label), 'w') as f:
-                f.writelines(modified_label_lines)
+                dest_dir = dataset_dir / dataset_directory / 'labels' / label_file.name
+                copy_or_move_files(label_file, dest_dir, copy_instead_move)
 
-            # move the labels to the corresponding directories
-            if dataset_directory == 'train':
-                os.rename(os.path.join(label_path, label), os.path.join(train_label_dir, label))
-            elif dataset_directory == 'test':
-                os.rename(os.path.join(label_path, label), os.path.join(test_label_dir, label))
-            elif dataset_directory == 'valid':
-                os.rename(os.path.join(label_path, label), os.path.join(valid_label_dir, label))
-    
-    # remove the old directories containing the emptited subdirectories
-    shutil.rmtree( Path(path).parent )
+        if not copy_instead_move:
+            shutil.rmtree(sub_dir.parent)
+            print(f"Removing directory {sub_dir.parent} with all its contents.")
 
-print('Successfully merged the datasets!')
+    print('Successfully merged the datasets!')
+
+if __name__ == "__main__":
+    main()
