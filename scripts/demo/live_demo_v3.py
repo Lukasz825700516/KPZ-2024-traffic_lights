f49040916ad9d674a6b151e2aba201257d5536ab
Iamhexi <igor.sosnowicz@gmail.com>
2024-04-19 18:18:32 +0200

    fix: use argument parser

--- scripts/demo/live_demo_v3.py
+++ scripts/demo/live_demo_v3.py
@@ -4,31 +4,27 @@ import torch
 import cv2
 import ultralytics
 import time
+import argparse
 from TrafficLights import TrafficLights
 
-def validate_arguments():
-    if len(sys.argv) < 3:
-        print("missing arguments: <weights> <source> <delay>=0.0s")
-        exit(1)
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="traffic_lights_demo",
+        description="Controls traffic lights using Deep Learning."
+    )
 
-    if not os.path.isfile(sys.argv[1]):
-        print(f"File {sys.argv[1]} not found")
-        exit(1)
+    parser = argparse.ArgumentParser(description='Controls traffic lights using Deep Learning.')
+    parser.add_argument('weights', type=str, help='File containing YOLO\'s weights and biases.')
+    parser.add_argument('source', type=str, help='Source of video stream such as a camera input or a file. In case of camera, use its id (usually 0). In case of a file, use its path.')
+    parser.add_argument('frame_duration', type=float, nargs='?', default=0.0, help='A duration of a frame in seconds.')
 
-    input_source = sys.argv[2]
-    if input_source.isdigit():
-        input_source = int(input_source)
-
-    delay = 0.0
-    if len(sys.argv) == 4:
-        delay = float(sys.argv[3])
-
-    return input_source, delay
+    return parser
 
 def main():
-    minimal_detection_confidence = 0.5
+    parser = create_parser()
+    args = parser.parse_args()
 
-    input_source, frame_duration = validate_arguments()
+    minimal_detection_confidence = 0.5
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = ultralytics.YOLO(sys.argv[1],)
@@ -37,7 +33,7 @@ def main():
     traffic_lights = TrafficLights()
     traffic_lights.start()
 
-    cap = cv2.VideoCapture(input_source)
+    cap = cv2.VideoCapture(args.source)
     while cap.isOpened():
         start_time = time.time()
 
@@ -53,15 +49,15 @@ def main():
         end_time = time.time()
         elapsed_time = end_time - start_time
 
-        if elapsed_time < frame_duration:
-            time.sleep(frame_duration - elapsed_time)
+        if elapsed_time < args.frame_duration:
+            time.sleep(args.frame_duration - elapsed_time)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
         detected_classes = [ int(detection.cls) for detection in predictions[0].boxes ]
 
-        traffic_lights.update(frame_duration,  detected_classes)
+        traffic_lights.update(args.frame_duration,  detected_classes)
         traffic_lights.display()
 
 
