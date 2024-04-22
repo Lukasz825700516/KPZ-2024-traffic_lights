dc02c9e1ed4063f115e2fb8ebdde203d65acf4ff
Iamhexi <igor.sosnowicz@gmail.com>
2024-04-22 23:09:08 +0200

    feat: add POSIX compliance

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -1,38 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-# Dataset directory
-dataset_path=Datasets
+dataset_path="Datasets"
 
-# Do not modify below
-dataset_path=`realpath $dataset_path`
+dataset_links="https://universe.roboflow.com/ds/Kl4NM4lIXU?key=zGmHdmdyxO \
+https://universe.roboflow.com/ds/veREHEjegj?key=GeS0UadfXA \
+https://universe.roboflow.com/ds/GGOHURobzV?key=GRIyo7vDjZ \
+https://universe.roboflow.com/ds/RFkCt0neqM?key=1cAkukn7Qu \
+https://universe.roboflow.com/ds/g2Rulh3EIN?key=cMHsiqOBCG"
+
+dataset_dirs="Blind \
+Stroller \
+Child_Elderly_Adult \
+Wheelchair \
+Suitcase"
+
 temporary_file=$dataset_path/tmp.zip
-rm $temporary_file -f
 
-dataset_links=( 
-    'https://universe.roboflow.com/ds/Kl4NM4lIXU?key=zGmHdmdyxO'
-    'https://universe.roboflow.com/ds/veREHEjegj?key=GeS0UadfXA'
-    'https://universe.roboflow.com/ds/GGOHURobzV?key=GRIyo7vDjZ'
-    'https://universe.roboflow.com/ds/RFkCt0neqM?key=1cAkukn7Qu'
-    'https://universe.roboflow.com/ds/g2Rulh3EIN?key=cMHsiqOBCG'
-)
-
-dataset_dirs=(
-    'Blind'
-    'Stroller'
-    'Child_Elderly_Adult'
-    'Wheelchair'
-    'Suitcase'
-)
-
-for (( i = 0; i < ${#dataset_links[@]}; i++ )); do
-    directory=${dataset_dirs[$i]}
-    dataset_link=${dataset_links[$i]}
-
-    curl -L "$dataset_link" > $temporary_file
-    unzip -n $temporary_file -d $dataset_path/$directory
-    rm $temporary_file
+i=1
+for link in $dataset_links; do
+    directory=$(echo "$dataset_dirs" | cut -d ' ' -f "$i")
+    curl -L "$link" > "$temporary_file"
+    unzip -n "$temporary_file" -d "$dataset_path/$directory"
+    i=$((i + 1))
 done
 
-rm README.dataset.txt README.roboflow.txt
+rm "$temporary_file"
+rm README.dataset.txt README.roboflow.txt -f
 
 echo "Successfully downloaded all datasets!"
