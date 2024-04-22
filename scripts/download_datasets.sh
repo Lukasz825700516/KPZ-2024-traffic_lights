05f38184593242cd0f4fcd3e5306eb91efaf9f91
Iamhexi <igor.sosnowicz@gmail.com>
2024-04-22 17:35:06 +0200

    fix: put datasets in corresponding subdirs

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -16,11 +16,23 @@ dataset_links=(
     'https://universe.roboflow.com/ds/g2Rulh3EIN?key=cMHsiqOBCG'
 )
 
+dataset_dirs=(
+    'Blind'
+    'Stroller'
+    'Child_Elderly_Adult'
+    'Wheelchair'
+    'Suitcase'
+)
 
-for dataset_link in "${dataset_links[@]}"; do
+for (( i = 0; i < ${#dataset_links[@]}; i++ )); do
+    directory=${dataset_dirs[$i]}
+    dataset_link=${dataset_links[$i]}
+
     curl -L "$dataset_link" > $temporary_file
-    unzip -n $temporary_file -d "$dataset_path"
+    unzip -n $temporary_file -d $dataset_path/$directory
     rm $temporary_file
 done
+
+rm README.dataset.txt README.roboflow.txt
 
 echo "Successfully downloaded all datasets!"
