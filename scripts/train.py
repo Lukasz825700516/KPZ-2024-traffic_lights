f3d8f75bc5c151c265da0a00719535a9e0448462
Iamhexi <igor.sosnowicz@gmail.com>
2024-04-27 00:44:44 +0200

    feat: partially develop local training, handle more params

--- scripts/train.py
+++ scripts/train.py
@@ -2,20 +2,29 @@ import os
 import argparse
 from typing import Dict
 import dotenv
+import pathlib
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
         prog='train',
-        description='The script trains the YOLO model placing artifacts as weights and results the proper places.'
+        description='The script trains the YOLO model placing artifacts as weights and results in the proper locations.'
     )
-
+    
+    parser.add_argument('-m', '--memory', help='Defines the maximum allowed memory allocation on GPU. 0 means dynamic. If an error occurs such as "torch.cuda.OutOfMemoryError: CUDA out of memory.", decrease this value.', default=0, type=int)
+    parser.add_argument('-g', '--goal', choices=['transfer-learning', 'fine-tuning', 'from-scratch'], default="transfer-learning", help='Choose whether to train all layers (learning-from-scratch), a few last layers (transfer-learning) or the very last layer (fine-tuning).')    
     parser.add_argument('-l', '--location', choices=["remote", "local"], help="Specify whether to train remotely or locally.", default="local")
     
+    parser.add_argument('-d', '--data', type=pathlib.Path, required=True, help="The path to the data.yaml file, which defines a dataset structure.")
+    parser.add_argument('-v', '--verbose', type=bool, default=False, help="Specifies if training should print a lot of details (True) or minimum (False).")
+    
     return parser.parse_args()
+    
 
 def remove_non_yolo_arguments(all_args):
     project_specific_args = [
-        'location'
+        'location',
+        'goal',
+        'memory'
     ]
 
     all_args = vars(all_args)
@@ -23,12 +32,51 @@ def remove_non_yolo_arguments(all_args):
         
     return yolo_args
 
-def train_locally(yolo_args: Dict[str, str]):
+def train_locally(yolo_args: Dict[str, str], goal: str, memory: str):
     print("Training locally")
+
+    # TODO: verify if yolo command is available in PATH
+
+
+    preset_args = {
+        'patience': 10,
+        'device': 0, # enforce GPU instead of CPU
+        'epochs': 9999 # train until 
+    }
+    
+    match goal:
+        case 'transfer-learning':
+            preset_args['freeze'] = "19"
+
+        case 'fine-tuning':
+            preset_args['freeze'] = "22"
+
+        case 'from-scratch':
+            pass
+            
+        case _:
+            print(f"Invalid value of the parameter 'goal', which is '{goal}'")
+            exit(1)
+
+    if memory == 0:
+        preset_args["batch"] = -1  # enable dynamic memory allocation on GPU
+    else:
+        images_per_gb = 16
+        preset_args["batch"] = memory * images_per_gb
+        
+    yolo_args = {**yolo_args, **preset_args}
+    
+    args = ' '
+    for key, value in yolo_args.items():
+        value = str(value)
+        args += f'{key}={value} '
+
+    print(f'yolo detect train {args}')
+    os.system(f'yolo {args}')
     # pass yolo args to command
     # save with an appropriate framework
 
-def train_remotely(yolo_args: Dict[str, str]):
+def train_remotely(yolo_args: Dict[str, str], goal: str, memory: str):
     print("Training remotely")
     credentials = dotenv.dotenv_values(".env")
     # upload dataset if not exists
@@ -42,10 +90,10 @@ def main():
 
     match args.location:
         case "local": 
-            train_locally(yolo_args)
+            train_locally(yolo_args, args.goal, args.memory)
 
         case "remote":
-            train_remotely(yolo_args)
+            train_remotely(yolo_args, args.goal, args.memory)
     
 
 if __name__ == '__main__':
