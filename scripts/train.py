13fb386b1c5cab661d3003ee960613fe81c4a17f
Iamhexi <igor.sosnowicz@gmail.com>
2024-04-27 22:17:22 +0200

    fix: refactor train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,16 +1,20 @@
-import os
+'''
+    The script to train YOLO8n model and logs results to MLFlow.
+'''
+
+import sys
+import pathlib
 import argparse
 from typing import Dict
-import dotenv
-import pathlib
-import subprocess
-from ultralytics import YOLO
 import mlflow
-
+from ultralytics import YOLO
 
 def print_warning(text: str) -> None:
-    print("\033[93m{}\033[0m".format(text))
-    
+    print(f'\033[93m{text}\033[0m')
+
+def print_error_and_exit(text: str) -> None:
+    print(f'\033[93m{text}\033[0m')
+    sys.exit(1)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -20,14 +24,13 @@ def parse_arguments():
 
     parser.add_argument('-e', '--epochs', default=0, type=int, help="Set the number of training epochs. It overrides the default behaviour (value of `0`), which is training ended when no improvement occurs within 10 epochs.", )
     parser.add_argument('-m', '--memory', help='Defines the maximum allowed memory allocation on GPU. 0 means dynamic. If an error occurs such as "torch.cuda.OutOfMemoryError: CUDA out of memory.", decrease this value.', default=0, type=int)
-    parser.add_argument('-g', '--goal', choices=['transfer-learning', 'fine-tuning', 'from-scratch'], default="transfer-learning", help='Choose whether to train all layers (learning-from-scratch), a few last layers (transfer-learning) or the very last layer (fine-tuning).')    
+    parser.add_argument('-g', '--goal', choices=['transfer-learning', 'fine-tuning', 'from-scratch'], default="transfer-learning", help='Choose whether to train all layers (learning-from-scratch), a few last layers (transfer-learning) or the very last layer (fine-tuning).')
     parser.add_argument('-t', '--task', choices=['classify', 'detect'], help="Determine whether to train the model for classification or object detection.")
         
     parser.add_argument('-d', '--data', type=pathlib.Path, required=True, help="The path to the data.yaml file, which defines a dataset structure.")
     parser.add_argument('-v', '--verbose', type=bool, default=False, help="Specifies if training should print a lot of details (True) or minimum (False).")
     
-    return parser.parse_args()
-    
+    return parser.parse_args()   
 
 def remove_non_yolo_arguments(all_args):
     project_specific_args = [
@@ -38,24 +41,28 @@ def remove_non_yolo_arguments(all_args):
     ]
 
     all_args = vars(all_args)
-    yolo_args = { arg_name: arg_value for arg_name, arg_value in all_args.items() if arg_name not in project_specific_args }
-        
+    yolo_args = {
+        arg_name: arg_value
+        for arg_name, arg_value in all_args.items()
+        if arg_name not in project_specific_args
+    }
+
     return yolo_args
 
 
 def save_results(hyperparameters: Dict[str, str], model, metrics, results, task: str, goal: str) -> None:
     host = '127.0.0.1'
     port = '8080'
-    mlflow.set_tracking_uri(uri='http://{host}:{port}')
+    mlflow.set_tracking_uri(uri=f'http://{host}:{port}')
 
     mlflow.set_experiment(f'yolo-{task}')
     with mlflow.start_run():
 
         mlflow.set_tag('Goal', goal)
-        signature = infer_signature(hyperparameters['data'], results)
+        signature = mlflow.infer_signature(hyperparameters['data'], results)
 
         for key, value in metrics.results_dict.items():
-                mlflow.log_metric(key, value)
+            mlflow.log_metric(key, value)
 
         mlflow.log_params(hyperparameters)
         mlflow.log_param("ap_class_index", metrics.ap_class_index.tolist())
@@ -65,13 +72,13 @@ def save_results(hyperparameters: Dict[str, str], model, metrics, results, task:
         mlflow.log_param("speed", metrics.speed)
         mlflow.log_param("task", metrics.task)
 
-        model_info = mlflow.pytorch.log_model(
+        mlflow.pytorch.log_model(
             pytorch_model=model,
             artifact_path='last_run/',
             signature=signature,
             registered_model_name='YOLO8n',
         )
-        
+     
 
 def train_locally(yolo_args: Dict[str, str], goal: str, memory: str, task: str):
     print("Training locally")
@@ -84,7 +91,7 @@ def train_locally(yolo_args: Dict[str, str], goal: str, memory: str, task: str):
     }
 
     del yolo_args['epochs'] # already used in preset_args
-    
+
     match goal:
         case 'transfer-learning':
             preset_args['freeze'] = 19
@@ -96,8 +103,7 @@ def train_locally(yolo_args: Dict[str, str], goal: str, memory: str, task: str):
             pass
             
         case _:
-            print(f"Invalid value of the parameter 'goal', which is '{goal}'")
-            exit(1)
+            print_error_and_exit(f"Invalid value of the parameter 'goal', which is '{goal}'")
 
     if memory == 0:
         preset_args["batch"] = -1  # enable dynamic memory allocation on GPU
@@ -106,20 +112,18 @@ def train_locally(yolo_args: Dict[str, str], goal: str, memory: str, task: str):
         preset_args["batch"] = memory * images_per_gb
 
     yolo_args = {**yolo_args, **preset_args}
-    
-    model = YOLO('yolov8n.pt')  
+
+    model = YOLO('yolov8n.pt')
     results = model.train(**yolo_args)
     metrics = model.val(data=yolo_args['data'])
 
     save_results(yolo_args, model, metrics, results, task, goal)
 
-    
+
 def main():
     args = parse_arguments()
-    yolo_args = remove_non_yolo_arguments(args) 
+    yolo_args = remove_non_yolo_arguments(args)
     train_locally(yolo_args, args.goal, args.memory, args.task)
 
 if __name__ == '__main__':
     main()
-    
-
