34e0e4105e7eb599ad347ba58033b32c44447e55
Iamhexi <igor.sosnowicz@gmail.com>
2024-05-14 09:06:09 +0200

    feat: develop timer, integrate with YOLO

---
+++ scripts/demo/counter/TrafficLights.py
@@ -0,0 +1,81 @@
+from LightState import LightState
+import os
+
+class TrafficLights:
+    def __init__(self):
+        self.__base_green_duration = 15
+        self.__base_red_duration = 15
+        self.__next_green_bouns_time = 0
+
+        self.__state = LightState.GREEN
+        self.__time_left = self.__base_green_duration
+
+        self.__is_running = False
+
+        self.__time_increment = { # in seconds
+            "Elderly" : 9,
+            "Child" : 9,
+            "Adult" : 0,
+            "Wheelchair" : 15,
+            "Blind" : 15,
+            "Suitcase" : 12,
+        }
+
+        self.__classes =  list( self.__time_increment.keys() )
+        self.__already_detected = {class_name: False for class_name in self.__classes}
+
+    def __enter__(self) -> 'TrafficLights':
+        self.__is_running = True
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback) -> None:
+        self.__is_running = False
+
+    def __update_state(self, elapsed_time: float) -> None:
+        self.__time_left -= elapsed_time
+
+        if self.__time_left > 0:
+            return
+
+        match self.__state:
+            case LightState.GREEN:
+                self.__state = LightState.RED
+                self.__time_left = self.__base_red_duration
+            case LightState.RED:
+                self.__state = LightState.GREEN
+                self.__time_left = self.__base_green_duration + self.__next_green_bouns_time
+                self.__next_green_bouns_time = 0
+                self.__already_detected = {class_name: False for class_name in self.__classes}
+            case other:
+                raise Exception(f"Unexpected state of TrafficLights: {other}")
+
+    def __extend_time_after_detection(self, detected_classes: list[int]) -> None:
+         for class_id in detected_classes:
+
+            class_name = self.__convert_class_ID_to_name(class_id)
+
+            if self.__already_detected[ class_name ]:
+                continue
+
+            match self.__state:
+                case LightState.GREEN:
+                    self.__time_left += self.__time_increment[ class_name ]
+                    self.__already_detected[ class_name ] = True
+                case LightState.RED:
+                    self.__next_green_bouns_time += self.__time_increment[ class_name ]
+
+    def __convert_class_ID_to_name(self, class_id: int) -> str:
+        return self.__classes[ class_id ]
+
+    def update(self, elapsed_time: float, classes_detected: list[int]) -> None:
+        if not self.__is_running:
+            raise RuntimeWarning('Cannot update state of the TrafficLights object, which is NOT running. Use `with TrafficLights() as tf:` instead.')
+
+        self.__update_state(elapsed_time)
+        self.__extend_time_after_detection(classes_detected)
+
+    def get_state(self) -> LightState:
+        return self.__state
+
+    def get_time_left(self) -> int:
+        return int(self.__time_left)
--- scripts/demo/counter/counter.py
+++ scripts/demo/counter/counter.py
@@ -1,51 +1,118 @@
+import argparse
+import torch
+import cv2
+import ultralytics
 import pygame
 from pygame import gfxdraw
+from TrafficLights import TrafficLights
+from LightState import LightState
 
-framerate = 60
+MINIMAL_DETECTION_CONFIDENCE = 0.5
+IOU = 0.5
 window_size = (700, 700)
-time_left = 60
 background_color = pygame.Color(255, 255, 255)
+
 
 def draw_light(screen, position, color: pygame.Color) -> None:
     gfxdraw.aacircle(screen, *position, 120, color)
     gfxdraw.filled_circle(screen, *position, 120, color)
 
+
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="traffic_lights_demo",
+        description="Controls traffic lights using Deep Learning."
+    )
+
+    parser = argparse.ArgumentParser(description='Controls traffic lights using Deep Learning.')
+    parser.add_argument('weights', type=str, help='File containing YOLO\'s weights and biases.')
+    parser.add_argument('source', type=str,
+        help='Source of video stream such as a camera input or a file. \
+        In case of camera, use its id (usually 0). In case of a file, use its path.')
+    parser.add_argument('framerate', type=float, nargs='?', default=60,
+        help='A minimal duration of a frame in seconds. \
+        Notice that processing a frame and inference may take longer than this.')
+
+    return parser
+
+
 def main() -> None:
+
     pygame.init()
     if not pygame.font:
         raise Warning("Fonts are disabled.")
 
-    screen = pygame.display.set_mode(window_size)
-    clock = pygame.time.Clock()
-    running = True
-    
-    if pygame.font:
-        background = pygame.Surface(screen.get_size())
-        background = background.convert()
-        background.fill(background_color)
-        font = pygame.font.Font(None, 64)
-    
+    parser = create_parser()
+    args = parser.parse_args()
 
-    window_center = ( int(window_size[0]/2), int(window_size[1]/2) )
+    device = torch.device(
+        "cuda:0" if torch.cuda.is_available()
+        else "cpu"
+    )
 
-    while running:
+    model = ultralytics.YOLO(args.weights,)
+    model = model.to(device)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    with TrafficLights() as traffic_lights:
+
+        if args.source.isdigit():
+            source = int(args.source)
+        else:
+            source = args.source
+
+        screen = pygame.display.set_mode(window_size)
+        clock = pygame.time.Clock()
+        running = True
+
+        if pygame.font:
+            background = pygame.Surface(screen.get_size())
+            background = background.convert()
+            background.fill(background_color)
+            font = pygame.font.Font(None, 64)
+
+        window_center = ( int(window_size[0]/2), int(window_size[1]/2) )
+
+        while running:
+
+            cap = cv2.VideoCapture(source)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            frame_returned, frame = cap.read()
+
+            if not frame_returned:
+                running = False
+            elif pygame.key.get_pressed()[pygame.K_q]:
                 running = False
 
-        screen.fill(background_color)
-        
-        text = font.render(f'Timer: {time_left} s', True, (10, 10, 10))
-        textpos = text.get_rect(centerx=background.get_width() / 2, y=10)
-        background.blit(text, textpos)
-        screen.blit(background, (0, 0))
+            predictions = model(frame, verbose=False, conf=MINIMAL_DETECTION_CONFIDENCE, iou=IOU)
+            detected_classes = [ int(detection.cls) for detection in predictions[0].boxes ]
+            traffic_lights.update(1/args.framerate,  detected_classes)
 
-        # Rendering
-        draw_light(screen=screen, position=window_center, color=pygame.Color(255, 0, 0))
+            light_color = pygame.Color(0, 0, 0)
+            match traffic_lights.get_state():
+                case LightState.RED:
+                    light_color = pygame.Color(255, 0, 0)
 
-        pygame.display.flip()
-        clock.tick(framerate)
+                case LightState.GREEN:
+                    light_color = pygame.Color(0, 255, 0)
+
+                case other:
+                    raise Warning(f'Unknown color of the traffic light: {other}.')
+
+            screen.fill(background_color)
+
+            text = font.render(f'Timer: {traffic_lights.get_time_left()} s', True, (10, 10, 10))
+            screen.blit(text, (10, 10))
+
+            draw_light(screen=screen, position=window_center, color=light_color)
+
+            pygame.display.flip()
+            clock.tick(args.framerate)
+
+        cap.release()
+
 
 if __name__ == '__main__':
     main()
